9829681e0a11a05d7f79fb24298efc82bd414b7e
손명수 <sonmyeongsu@sonmyeongsus-MacBook-Pro.local>
2020-05-15 18:23:30 +0900

    2020. 05. 15 -1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const foodList = [
+  {
+    id: 1,
+    name: '김밥',
+    url: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/04/08/1d26c0444e724bca8ed271b24da0057a1.jpg',
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: '떡볶이',
+    url: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/10/07/f43063e718c49d85ddce4880e4a41fcd1.jpg',
+    rating: 4.7,
+  },
+]
+
+function Food({ favorite, picture, rating }){
+  return (
+    <div>
+      <h1>I love { favorite }</h1>
+      <h1>{rating}/5.0</h1>
+      <img src={picture} alt={favorite} />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  favorite: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello!!!
+      {
+        foodList.map( obj=>(
+          <Food key={obj.id} favorite={obj.name} picture={obj.url} rating={obj.rating}/>
+        ))
+      }
     </div>
   );
 }
