16cb025294e85dd0a2740edf25f02a8cd4801dc8
손명수 <sonmyeongsu@sonmyeongsus-MacBook-Pro.local>
2020-05-15 20:04:16 +0900

    2020. 05. 15. -2

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React from 'react';
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 
-const foodList = [
-  {
-    id: 1,
-    name: '김밥',
-    url: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/04/08/1d26c0444e724bca8ed271b24da0057a1.jpg',
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    name: '떡볶이',
-    url: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/10/07/f43063e718c49d85ddce4880e4a41fcd1.jpg',
-    rating: 4.7,
-  },
-]
+class App extends React.Component{
+  state = {
+    isLoading: true,
+  }
 
-function Food({ favorite, picture, rating }){
-  return (
-    <div>
-      <h1>I love { favorite }</h1>
-      <h1>{rating}/5.0</h1>
-      <img src={picture} alt={favorite} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  favorite: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
-  return (
-    <div>
-      Hello!!!
-      {
-        foodList.map( obj=>(
-          <Food key={obj.id} favorite={obj.name} picture={obj.url} rating={obj.rating}/>
-        ))
-      }
-    </div>
-  );
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        isLoading: false
+      });
+    }, 6000);
+  }
+  
+  render(){
+    const { isLoading } = this.state;
+    return (
+      <div>
+        <span>{ isLoading ? "loading..." : "completed!!!" }</span>
+      </div>
+    );
+  }
 }
 
 export default App;
